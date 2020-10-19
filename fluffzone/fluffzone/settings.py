bb39f59e67dc314366a02e8cd4a1bcb1914e5cb8
Chiraayu_Pravin <chiraayupm@gmail.com>
2020-10-20 00:16:36 +0530

    add secret key

--- fluffzone/fluffzone/settings.py
+++ fluffzone/fluffzone/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '^-0olon!w@-$d-%tha^*x5y=!40f&7sq_k8ga09eu1p(yfscqr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
