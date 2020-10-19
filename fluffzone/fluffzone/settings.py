d643edeeae95039c67f221d67215ba2736772e28
Chiraayu_Pravin <chiraayupm@gmail.com>
2020-10-20 01:08:05 +0530

    update requirements and settings

--- fluffzone/fluffzone/settings.py
+++ fluffzone/fluffzone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 from decouple import config
 
@@ -127,3 +127,4 @@ STATIC_ROOT = BASE_DIR/ 'staicfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/ 'media'
 
+django_heroku.settings(locals())
