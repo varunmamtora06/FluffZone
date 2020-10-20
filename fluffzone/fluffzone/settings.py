2e86936cf25b3a0b2dbdf4d823cc9239fb55bbbc
Chiraayu_Pravin <chiraayupm@gmail.com>
2020-10-20 18:59:18 +0530

    updates

--- fluffzone/fluffzone/settings.py
+++ fluffzone/fluffzone/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     }
 }
 
-
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
